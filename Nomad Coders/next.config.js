e1f0b6f7532beb82da04163235aea290d4c65064
g9h22 <g9h22@naver.com>
2023-10-15 00:09:07 +0900

    Add Rewrite

--- Nomad Coders/next.config.js
+++ Nomad Coders/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form", // 재접속 할 주소 (외부주소도 가능)
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
