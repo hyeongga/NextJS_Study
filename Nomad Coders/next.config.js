f9a9ecfbd65d37e48f6e928f181f431e623d1191
g9h22 <g9h22@naver.com>
2023-10-17 05:46:06 +0900

    Redirect and Rewrite

--- Nomad Coders/next.config.js
+++ Nomad Coders/next.config.js
@@ -1,12 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
       {
-        source: "/contact",
-        destination: "/form", // 재접속 할 주소 (외부주소도 가능)
+        source: "/old-blog/:path*",
+        destination: "/new-blow/:path*", // 재접속 할 주소 (:path*의 경우, 뒤어 어떤 값이 와도 중간 경로만 변함)
         permanent: false,
+      },
+      {
+        source: "/google",
+        destination: "https://www.google.com/", //외부 주소도 가능
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
       },
     ];
   },
--- Nomad Coders/pages/index.js
+++ Nomad Coders/pages/index.js
@@ -5,17 +5,11 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
       console.log(results);
     })();
