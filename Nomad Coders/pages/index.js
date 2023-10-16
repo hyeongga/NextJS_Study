84ef47a6b2e3b228c98a15aaa7d2099da76bfc58
g9h22 <g9h22@naver.com>
2023-10-17 06:23:22 +0900

    Server side rendering

--- Nomad Coders/pages/index.js
+++ Nomad Coders/pages/index.js
@@ -5,21 +5,11 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-      console.log(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home" />
-      {!movies && <h4>...Loading</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -48,4 +38,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+/*getServerSideProps 함수명 변경 불가 ; 서버에서 작동하는 코드(client에 표기되지 않음)*/
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
